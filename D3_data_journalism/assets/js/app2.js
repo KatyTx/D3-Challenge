537ecc4c7b95224e45466afe5d167f2944e76fe3
B M <adjunctfuentes@gmail.com>
2020-11-19 18:36:16 -0600

    first draft of code

--- D3_data_journalism/assets/js/app2.js
+++ D3_data_journalism/assets/js/app2.js
@@ -16,7 +16,7 @@ var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Select body, append SVG area to it, and set the dimensions
 var svg = d3
-    .select("#scatter")
+    .select("scatter")
     .append("svg")
     .attr("height", svgHeight)
     .attr("width", svgWidth);
@@ -31,7 +31,7 @@ var chartGroup = svg.append("g")
 
 d3.csv("assets/data/data.csv").then(function (demoData) {
 
-    // Print the Demographics Data on console to ensure ok
+    // Print the Demographics Data
     console.log(demoData);
 
     //extract data for analysis
@@ -43,16 +43,24 @@ d3.csv("assets/data/data.csv").then(function (demoData) {
         data.smokes = +data.smokes
         data.age = +data.age
 
-        //test data extraction is ok on console
         console.log(data.state);
     });
 
+    //scale functions
     var xLinearScale = d3.scaleLinear()
         .domain([20, d3.max(demoData, d => d.healthcare)])
         .range([0, chartWidth]);
 
+    var xLinearScale2 = d3.scaleLinear()
+        .domain([20, d3.max(demoData, d => d.smokes)])
+        .range([0, chartWidth]);
+
     var yLinearScale = d3.scaleLinear()
         .domain([0, d3.max(demoData, d => d.poverty)])
+        .range([chartHeight, 0]);
+
+    var yLinearScale2 = d3.scaleLinear()
+        .domain([0, d3.max(demoData, d => d.age)])
         .range([chartHeight, 0]);
 
     //  axis functions
@@ -66,4 +74,35 @@ d3.csv("assets/data/data.csv").then(function (demoData) {
 
     chartGroup.append("g")
         .call(leftAxis);
+
+    //create circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(demoData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.healthcare))
+        .attr("cy", d => yLinearScale(d.poverty))
+        .attr("r", "15")
+        .attr("fill", "pink")
+        .attr("opacity", ".5");
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - chartMargin.left + 40)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Healthcare vs. Poverty");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Smokers vs. Age");
+
+}).catch(function (error) {
+    console.log(error);
+
+
+
 });
